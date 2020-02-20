40812d1382654e3f9654721a2e41600cf8fe87c2
Henrynunez112 <henrynunez@pursuit.org>
2020-02-20 11:50:17 -0500

    just needs to do css

--- gofundme/src/App.js
+++ gofundme/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import Donations from './component/Donations'
+import Header from './component/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Donations />
     </div>
   );
---
+++ gofundme/src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>GO Fund Me</h1>
+                <p>Help me go on vacation</p>
+            </div>
+        )
+    }
+}
+
+export default Header
